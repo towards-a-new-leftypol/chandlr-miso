77ef4b23ee2e2529fd67b4c2b40242a612d3c5f5
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2025-07-06 03:48:31 -0400

    Hardcode CatalogPost item to skip XHR
    - shows the bug, so it's not our client component

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,6 @@
 module Main where
 
 import Data.Proxy
-import Data.Maybe (fromJust)
 import Data.Text (Text)
 import Data.Text.Encoding (encodeUtf8)
 import qualified Data.Text as T
@@ -47,6 +46,7 @@ import Data.Aeson (decodeStrict, FromJSON)
 import Control.Monad.IO.Class (liftIO)
 import Language.Javascript.JSaddle (toJSString)
 import Language.Javascript.JSaddle.Monad (JSM)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 
 import Common.FrontEnd.Action
 import Common.FrontEnd.Routes
@@ -68,6 +68,8 @@ import JSFFI.Saddle
     , querySelector
     , getAttribute
     )
+import qualified Common.Network.CatalogPostType as CatP
+import qualified Common.Network.HttpTypes as Http
 
 data InitialData
     = CatalogData [ CatalogPost ]
@@ -84,7 +86,8 @@ parseInitialDataUsingRoute model uri raw_json = either (const Nil) id routing_re
 
         routing_result = route (Proxy :: Proxy Route) handlers (const uri) model
 
-        handlers = h_latest :<|> h_thread :<|> h_search
+        -- handlers = h_latest :<|> h_thread :<|> h_search
+        handlers = h_latest
 
         h_latest :: Model -> InitialData
         h_latest _ = CatalogData $ maybe ([]) id $ decoded_thing
@@ -100,7 +103,8 @@ initialActionFromRoute model uri = either (const NoAction) id routing_result
     where
         routing_result = route (Proxy :: Proxy Route) handlers (const uri) model
 
-        handlers = h_latest :<|> h_thread :<|> h_search
+        -- handlers = h_latest :<|> h_thread :<|> h_search
+        handlers = h_latest
 
         h_latest :: Model -> Action
         h_latest = const $ GoToTime $ current_time model
@@ -133,6 +137,7 @@ initialModel pgroot fetch_count media_root u t smv = Model
     , search_term = ""
     , initial_action = NoAction
     , thread_action = Nothing
+    , grid_action = Nothing
     , pg_api_root = pgroot
     , client_fetch_count = fetch_count
     }
@@ -156,6 +161,9 @@ foreign export javascript "hs_start" main :: IO ()
 
 main :: IO ()
 main = run mainMain
+
+epochToUTCTime :: Int -> UTCTime
+epochToUTCTime = posixSecondsToUTCTime . realToFrac
 
 mainMain :: JSM ()
 mainMain = do
@@ -195,14 +203,41 @@ mainMain = do
           now
           search_var
 
+    let testPost = CatP.CatalogPost
+            { CatP.post_id              = Just 0
+            , CatP.board_post_id        = 0
+            , CatP.board_thread_id      = 0
+            , CatP.creation_time        = epochToUTCTime 0
+            , CatP.bump_time            = epochToUTCTime 0
+            , CatP.body                 = Just "Holy Shit"
+            , CatP.name                 = Nothing
+            , CatP.subject              = Nothing
+            , CatP.email                = Nothing
+            , CatP.thread_id            = 0
+            , CatP.embed                = Nothing
+            , CatP.estimated_post_count = 0
+            , CatP.site_name            = "test"
+            , CatP.pathpart             = "b"
+            , CatP.file_mimetype        = Nothing
+            , CatP.file_illegal         = Nothing
+            , CatP.file_name            = Nothing
+            , CatP.file_extension       = Nothing
+            , CatP.file_thumb_extension = Nothing
+            }
+
+        -- HaveLatest (HttpResult [ CatalogPost ])
+
+    let initAction = HaveLatest $ Http.HttpResponse 100 "OK" (Just [testPost])
+
     let app :: MainComponent = Component
-            { model         = initial_model { initial_action = initialActionFromRoute initial_model uri }
+            -- { model         = initial_model { initial_action = (initialActionFromRoute initial_model uri) }
+            { model         = initial_model
             , update        = mainUpdate app
             , view          = mainView app
             , subs          = [ uriSub ChangeURI ]
             , events        = defaultEvents
             , styles = []
-            , initialAction = Nothing
+            , initialAction = Just initAction
             , mountPoint    = Nothing
             , logLevel      = DebugAll
             }
@@ -218,7 +253,7 @@ addToView _ v = v
 mainView :: MainComponent -> Model -> View Action
 mainView mc model = view
     where
-        view = either (const page404) addClient $
+        view = either (const page404) id $
             route (Proxy :: Proxy Route) handlers current_uri model
 
         addClient :: View Action -> View Action
@@ -231,8 +266,8 @@ mainView mc model = view
 
         handlers
             =    (catalogView tc grid)
-            :<|> (threadView Thread.app)
-            :<|> (searchView grid)
+            -- :<|> (threadView Thread.app)
+            -- :<|> (searchView grid)
 
         tc :: TC.TimeControl
         tc = TC.app 0 timeCallback
@@ -278,9 +313,13 @@ mainUpdate _ (HaveLatest (Client.HttpResponse {..})) =
     case body of
         Nothing -> io_ $
             consoleLog "Didn't get anything back from API"
-        Just posts -> io_ $
-            -- mapM_ (consoleLog . toJSString . show) posts
-            notify (Grid.app undefined undefined) $ Grid.DisplayItems posts
+        Just posts -> do
+            io_ $ do
+                mapM_ (consoleLog . toJSString . show) posts
+                -- notify (Grid.app undefined undefined) $ Grid.DisplayItems posts
+
+            -- here we need to modify our model to save these catalog posts as an action for Grid
+            modify (\m -> m { grid_action = Just $ Grid.DisplayItems posts })
 
 mainUpdate _ (HaveThread Client.Error) =
     io_ $ consoleLog "Getting Thread failed!"
@@ -327,8 +366,9 @@ mainUpdate mc (GetThread GetThreadArgs {..}) = do
             }
 
 mainUpdate _ (ChangeURI uri) = do
-    modify (\m -> m { current_uri = uri })
-    io_ $ consoleLog $ "ChangeURI! " `append` (pack $ show $ uri)
+    io_ $ consoleLog $ "mainUpdate - ChangeURI! " `append` (toMisoString $ show $ uri)
+    --modify (\m -> m { current_uri = current_uri m })
+    modify (\m -> m { current_uri = uri { uriPath = "/asdf" } })
 
 
 mainUpdate _ (SearchResults query) = do
@@ -350,3 +390,13 @@ mainUpdate _ (SearchResults query) = do
             }
 
 mainUpdate _ NoAction = io_ $ consoleLog "Main - NoAction"
+
+mainUpdate mc GridMounted = do
+    io_ $ consoleLog "Main - GridMounted"
+
+    model <- get
+
+    maybe
+        (return ())
+        (io_ . notify (Grid.app mc (media_root_ model)))
+        (grid_action model)
